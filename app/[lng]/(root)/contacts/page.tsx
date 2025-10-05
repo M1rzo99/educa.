c23fe14f6a8f584ea1f22dd661327448b393cde4
Mirzo <accaunoff99@gmail.com>
2025-10-05 14:40:08 +0900

    feat: added mapLocation

--- app/[lng]/(root)/contacts/page.tsx
+++ app/[lng]/(root)/contacts/page.tsx
@@ -17,7 +17,7 @@ async function Page({ params: { lng } }: LngParams) {
 		<>
 			<TopBar label='contacts' />
 			<iframe
-				src='https://maps.app.goo.gl/15wfzmfVAFKun6Fp6'
+				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d767609.1494093945!2d60.5430369873563!3d41.27680388760016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x41de3cbf7f89baef%3A0xe874bc482a2737e!2sXorazm%20Region%2C%20Uzbekistan!5e0!3m2!1sen!2skr!4v1759641984922!5m2!1sen!2skr"
 				loading='lazy'
 				className='h-96 w-full'
 			/>
